b0567e1131401e343721bf6752c477cce9bdd325
Luis Cabrera <loudigi@gmail.com>
2017-04-07 02:17:38 -0400

    Added Seed Data

---
+++ TravelOn/Migrations/201704070544393_AddedSeed.cs
@@ -0,0 +1,16 @@
+namespace TravelOn.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedSeed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- TravelOn/Migrations/Configuration.cs
+++ TravelOn/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace TravelOn.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TravelOn.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TravelOn.Models.TravelOnContext>
     {
@@ -26,6 +27,67 @@ namespace TravelOn.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Categories.AddOrUpdate(x => x.ID,
+       new Category() { ID = 20, Name = "Attractions" },
+       new Category() { ID = 21, Name = "Beaches" },
+       new Category() { ID = 22, Name = "Museum" },
+       new Category() { ID = 23, Name = "Hotel" }
+       );
+
+            context.Rates.AddOrUpdate(x => x.ID,
+new Rate() { ID = 20, Rating = 1 },
+new Rate() { ID = 21, Rating = 2 },
+new Rate() { ID = 22, Rating = 3 },
+new Rate() { ID = 23, Rating = 4 },
+new Rate() { ID = 24, Rating = 5 }
+);
+
+            context.Reviews.AddOrUpdate(x => x.ID,
+                new Review()
+                {
+                    ID = 20,
+                    Title = "Sandbox Easton",
+                    Published = DateTime.Parse(DateTime.Today.ToString()),
+                    CategoryID = 21,
+                    Content = "Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta.",
+                    RateID = 23,
+                    ImagePath = "https://media-cdn.tripadvisor.com/media/photo-o/09/82/90/09/siesta-beach.jpg"
+
+                },
+                new Review()
+                {
+                    ID = 21,
+                    Title = "The Pearfield",
+                    Published = DateTime.Parse(DateTime.Today.ToString()),
+                    CategoryID = 23,
+                    Content = "Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta.",
+                    RateID = 22,
+                    ImagePath = "https://t-ec.bstatic.com/images/hotel/max1024x768/176/17609411.jpg"
+
+                },
+                new Review()
+                {
+                    ID = 22,
+                    Title = "Steps to Heaven",
+                    Published = DateTime.Parse(DateTime.Today.ToString()),
+                    CategoryID = 20,
+                    Content = "Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta.",
+                    RateID = 21,
+                    ImagePath = "https://media-cdn.tripadvisor.com/media/photo-o/0e/99/92/d4/front-of-the-basilica.jpg"
+                },
+                new Review()
+                {
+                    ID = 23,
+                    Title = "Upstar Arts Collective",
+                    Published = DateTime.Parse(DateTime.Today.ToString()),
+                    CategoryID = 22,
+                    Content = "Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta.",
+                    RateID = 24,
+                    ImagePath = "https://media-cdn.tripadvisor.com/media/photo-o/05/6b/c7/ee/musee-du-louvre.jpg"
+                }
+                );
+
         }
     }
 }
